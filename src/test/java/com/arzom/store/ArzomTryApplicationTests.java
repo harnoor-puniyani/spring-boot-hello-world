febcf566e1393a816af0964a8be05428b93aa32e
harnoor <harnoor.puniyani@in.ey.com>
2025-05-25 22:07:31 +0530

    initial Commit

---
+++ src/test/java/com/arzom/store/ArzomTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arzom.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArzomTryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
