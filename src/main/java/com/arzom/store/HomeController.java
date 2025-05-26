c7fc8445ccfb0d160c58813301cb5570750673e0
harnoor <harnoor.puniyani@in.ey.com>
2025-05-26 08:17:59 +0530

    Added about and contact

--- src/main/java/com/arzom/store/HomeController.java
+++ src/main/java/com/arzom/store/HomeController.java
@@ -1,7 +1,9 @@
 package com.arzom.store;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -10,5 +12,22 @@ public class HomeController {
     public String Home()
     {
         return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    public String About()
+    {
+        return "Authored by Arzom";
+    }
+
+    @GetMapping("/contact")
+    @ResponseBody
+    public Object Contact()
+    {
+        return new Object(){
+            public String name = "Arzom";
+            public String email = "arzom@gmail.com";
+        };
     }
 }
